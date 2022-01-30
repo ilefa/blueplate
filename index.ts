3ef4a2de960c16de577deaaba1af048a460cb8d7
Mike M <mikester70387@gmail.com>
2022-01-29 21:17:23 -0500

    Version 1.0.5

--- index.ts
+++ index.ts
@@ -5,8 +5,7 @@ export type Meal = {
     stations: string[];
 }
 
-export type DiningHallResponse = {
-    hall: DiningHall;
+export type DiningHallResponse = DiningHall & {
     time: Date;
     meals: Meal[];
     type: keyof typeof DiningHallType;
@@ -21,6 +20,7 @@ export type DiningHall = {
         name: string;
         latitude: number;
         longitude: number;
+        address: string;
         maps: string;
     }
 }
@@ -55,6 +55,7 @@ export const DiningHalls: Record<keyof typeof DiningHallType, DiningHall> = {
             name: 'Buckley+Dining+Hall',
             latitude: 41.805625,
             longitude: -72.252385,
+            address: '1262 Storrs Rd, Storrs, CT, 06269',
             maps: 'https://www.google.com/maps/place/John+Buckley+Residence+Hall,+Storrs,+CT+06269/@41.8057388,-72.2460738,17z/data=!3m1!4b1!4m5!3m4!1s0x89e68a238c8f6bd7:0x234b2994a73acbaf!8m2!3d41.8057348!4d-72.2438851'
         }
     },
@@ -66,6 +67,7 @@ export const DiningHalls: Record<keyof typeof DiningHallType, DiningHall> = {
             name: 'McMahon+Dining+Hall',
             latitude: 41.803548,
             longitude: -72.252385,
+            address: '2011 Hillside Rd, Storrs, CT, 06269',
             maps: 'https://www.google.com/maps/place/McMahon+Dining+Hall/@41.8035583,-72.2546329,17z/data=!3m1!4b1!4m5!3m4!1s0x89e68a3d9478806f:0xc7f055938ed1d0a6!8m2!3d41.8033822!4d-72.2522574'
         }
     },
@@ -77,6 +79,7 @@ export const DiningHalls: Record<keyof typeof DiningHallType, DiningHall> = {
             name: 'North+Campus+Dining+Hall',
             latitude: 41.812185,
             longitude: -72.258381,
+            address: '82 N Eagleville Rd, Storrs, CT 06269',
             maps: 'https://www.google.com/maps/place/North+Campus+Dining+Hall/@41.8118133,-72.2580436,20.01z/data=!4m12!1m6!3m5!1s0x89e68a3d9478806f:0xc7f055938ed1d0a6!2sMcMahon+Dining+Hall!8m2!3d41.8033822!4d-72.2522574!3m4!1s0x0:0xb9f08e6948417f21!8m2!3d41.8121366!4d-72.2585661',
         }
     },
@@ -88,6 +91,7 @@ export const DiningHalls: Record<keyof typeof DiningHallType, DiningHall> = {
             name: 'Northwest+Marketplace',
             latitude: 41.811441,
             longitude: -72.259667,
+            address: 'N Eagleville Rd, Storrs, CT, 06269',
             maps: 'https://www.google.com/maps/place/Northwest+Dining+Hall/@41.8112212,-72.2599746,20.01z/data=!3m1!5s0x89e68a3879629c67:0x409c2613356c4ee1!4m5!3m4!1s0x0:0xe476d288c7ffdf1c!8m2!3d41.811443!4d-72.259743'
         }
     },
@@ -99,6 +103,7 @@ export const DiningHalls: Record<keyof typeof DiningHallType, DiningHall> = {
             name: 'Putnam+Dining+Hall',
             latitude: 41.805151,
             longitude: -72.258880,
+            address: '2358 Alumni Dr, Storrs, CT 06269',
             maps: 'https://www.google.com/maps/place/Putnam+Refectory/@41.8058232,-72.2599886,17.97z/data=!3m1!5s0x89e68a3f9416b0d3:0x4fa606fb32492bef!4m5!3m4!1s0x0:0xe102fa527107db81!8m2!3d41.805226!4d-72.2589772'
         }
     },
@@ -110,6 +115,7 @@ export const DiningHalls: Record<keyof typeof DiningHallType, DiningHall> = {
             name: 'South+Campus+Marketplace',
             latitude: 41.803892,
             longitude: -72.248538,
+            address: 'Lewis B. Rome Commons, Storrs, CT 06269',
             maps: 'https://www.google.com/maps/place/South+Campus+Dining+Hall/@41.8038295,-72.2503784,17.97z/data=!4m5!3m4!1s0x0:0x78bae9af27afcc79!8m2!3d41.8037265!4d-72.2486193'
         }
     },
@@ -121,6 +127,7 @@ export const DiningHalls: Record<keyof typeof DiningHallType, DiningHall> = {
             name: 'Gelfenbien+Commons+%26+Halal',
             latitude: 41.813455,
             longitude: -72.254368,
+            address: '3384 Tower Loop Rd, Storrs, CT 06269',
             maps: 'https://www.google.com/maps/place/41%C2%B048\'48.4%22N+72%C2%B015\'15.9%22W/@41.8135159,-72.253911,19.46z/data=!4m6!3m5!1s0x89e68a374c6ed731:0xbdf26ec0e2e34ec4!7e2!8m2!3d41.8134318!4d-72.2544378'
         }
     },
@@ -132,6 +139,7 @@ export const DiningHalls: Record<keyof typeof DiningHallType, DiningHall> = {
             name: 'Whitney+Dining+Hall',
             latitude: 41.809891,
             longitude: -72.247374,
+            address: '1356 Storrs Rd, Storrs, CT 06269',
             maps: 'https://www.google.com/maps/place/Edwina+Whitney+Residence+Hall/@41.8101301,-72.2475367,19z/data=!4m5!3m4!1s0x89e68a253a40d1e5:0xa1fbff3b6e368cf4!8m2!3d41.8099587!4d-72.2472346'
         }
     }
@@ -153,7 +161,10 @@ export const Weekends = [6, 0];
 export const getMenu = async (type: DiningHallType, date = new Date()): Promise<DiningHallResponse> => {
     let hall: DiningHall = DiningHalls[getEnumKeyByEnumValue(DiningHallType, type)];
     let url = `http://nutritionanalysis.dds.uconn.edu/shortmenu.aspx?sName=UCONN+Dining+Services&locationNum=${hall.location.id}&locationName=${hall.location.name}&naFlag=1`;
-    if (date.getDate() !== new Date().getDate())
+    let now = new Date();
+    if (date.getMonth() !== now.getMonth()
+        || date.getFullYear() !== now.getFullYear()
+        || date.getDate() !== new Date().getDate())
         url += `&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=${date.getMonth() + 1}%2f${date.getDate()}%2f${date.getFullYear()}`;
 
     return await axios
@@ -186,10 +197,11 @@ export const getDiningHallStatus = (type: DiningHallType, date = new Date()) => 
     if (hour < 7 || (hour < 8 && day === 0))
         return DiningHallStatus.CLOSED;
 
+    // Buckley closed on weekends
     if (isWeekend(day) && isDiningHall(type, DiningHallType.BUCKLEY))
         return DiningHallStatus.CLOSED;
 
-    // Before 9AM or Before 9:30AM on Weekdays - South serving breakfast, everything else closed
+    // Before 9AM or Before 9:30AM on weekends - South serving breakfast, everything else closed
     if (hour < 9 || (hour === 9 && minute < 30) && isWeekend(day)) {
         if (isDiningHall(type, DiningHallType.SOUTH))
             return DiningHallStatus.BREAKFAST;
